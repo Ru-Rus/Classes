42e01d2d9195f5f5560b580dcf23d44b9695282c
RMMarquillero <rmmarquillero@sscgi.com>
2025-09-16 11:24:31 +0800

    topic of Constructors and Inheritance
    Add Car class and refactor Vehicle initialization
    
    Introduced a new `Car` class that inherits from `Vehicle`, with a constructor that initializes the vehicle and outputs a message. Commented out several lines in `Program.cs` to disable original functionality for testing or refactoring, while adding an instance of `Car` to demonstrate its usage. Updated `Vehicle` class to enforce instantiation with a registration number by commenting out the default constructor.

--- AccessModifiers/Program.cs
+++ AccessModifiers/Program.cs
@@ -7,35 +7,38 @@ namespace AccessModifiers
 
         static void Main (string[] args)
         {
-            // This code is for Birthday ( Properties )
-            var person = new Person(new DateTime(2000, 12, 27));
-            Console.WriteLine(person.Age);
+            //// This code is for Birthday ( Properties )
+            //var person = new Person(new DateTime(2000, 12, 27));
+            //Console.WriteLine(person.Age);
 
 
-            // This code is for HttpCookie ( Indexers )
-            var cookie = new HttpCookie();
-            cookie["name"] = "Kaizer";
-            Console.WriteLine(cookie["name"]);
+            //// This code is for HttpCookie ( Indexers )
+            //var cookie = new HttpCookie();
+            //cookie["name"] = "Kaizer";
+            //Console.WriteLine(cookie["name"]);
 
-            //This Code is for Text and PresentationObject ( Inheritance )
-            var text = new Text();
-            text.Duplicate();
-            text.FontName = "Arial";
-            text.FontSize = 12;
-            text.Width = 100;
-            text.height = 150;
-            text.Copy();
+            ////This Code is for Text and PresentationObject ( Inheritance )
+            //var text = new Text();
+            //text.Duplicate();
+            //text.FontName = "Arial";
+            //text.FontSize = 12;
+            //text.Width = 100;
+            //text.height = 150;
+            //text.Copy();
 
-            //This code is for Logger and DbMigrator and Installer ( Composition )
-            var dbMigrator = new DbMigrator(new Logger());
+            ////This code is for Logger and DbMigrator and Installer ( Composition )
+            //var dbMigrator = new DbMigrator(new Logger());
 
-            var logger = new Logger();
-            var installer = new Installer(logger);
+            //var logger = new Logger();
+            //var installer = new Installer(logger);
 
-            dbMigrator.Migrate();
+            //dbMigrator.Migrate();
 
-            installer.Install();
+            //installer.Install();
             
+            //This code is for Car and Vehicle ( Constructors and Inheritance )
+            var car = new Car("ABC 123");
+
         }
 
     }
---
+++ AccessModifiers/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+   public class Vehicle
+    {
+        private readonly string _registrationNumber;
+
+        //public Vehicle()
+        //{
+        //    Console.WriteLine("Vehicle is being initialized");
+        //}
+
+        public Vehicle(string regNumber)
+        {
+            _registrationNumber = regNumber;
+
+            Console.WriteLine($"Vehicle is being initialized: {regNumber}");
+        }
+
+    }
+}
