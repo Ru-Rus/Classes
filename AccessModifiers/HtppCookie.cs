d90e454754ec12012beb4a4ad64fc38542fe7a12
RMMarquillero <rmmarquillero@sscgi.com>
2025-09-16 09:47:49 +0800

    Topic of Indexers
    Add HttpCookie class and usage example in Program.cs
    
    Implemented the HttpCookie class with methods for setting and retrieving cookie items, along with an indexer for easy access. Updated Program.cs to demonstrate the usage of the new HttpCookie class, while retaining existing commented-out code for "Birthday."

---
+++ AccessModifiers/HtppCookie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    public class HttpCookie {
+        private readonly Dictionary<string, string> _dic;
+        public DateTime expiry { get; set; }
+
+        public HttpCookie()
+        {
+            _dic = new Dictionary<string, string>();
+        }
+        public void SetItem(string key, string value) { 
+        
+        }
+        public string GetItem(string key) { 
+            return _dic[key];
+        }
+
+        public string this[string key]
+        {
+            get { return _dic[key]; }
+            set { _dic[key] = value; }
+        }
+
+    }
+}
--- AccessModifiers/Program.cs
+++ AccessModifiers/Program.cs
@@ -7,9 +7,15 @@ namespace AccessModifiers
 
         static void Main (string[] args)
         {
+            // This code is for Birthday
             var person = new Person(new DateTime(2000, 12, 27));
             Console.WriteLine(person.Age);
-            //
+
+
+            // This code is for HttpCookie
+            var cookie = new HttpCookie();
+            cookie["name"] = "Kaizer";
+            Console.WriteLine(cookie["name"]);
         }
 
     }
