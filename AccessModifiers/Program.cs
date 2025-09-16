41a7b07f0d4e870a41060aa3951b0cd76ca2022c
RMMarquillero <rmmarquillero@sscgi.com>
2025-09-16 09:09:37 +0800

    Add project files.

---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    public class Program
+    {
+
+        static void Main (string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(2000, 7, 27));
+            Console.WriteLine(person.GetBirthdate());
+        }
+
+    }
+}
